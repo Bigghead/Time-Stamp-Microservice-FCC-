fb879274543835e310f4f5b08080745a9fb42e1f
Shav <shavenchew@yahoo.com>
2016-11-07 16:57:59 -0800

    'Unix as parameter is working'

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/:timeStamp', function(req, res){
   var returnedObj = {
-    natural : null
+    natural : null,
+    unix : null
   };
-  var time = (req.params.timeStamp).replace(/&/g," ");
+  var time = (req.params.timeStamp).replace(/&/g," ");//.replace(/,/g, "");
+  if(moment(time, 'MMMM Do, YYYY').isValid()){
+    returnedObj.natural = time;
+    returnedObj.unix = moment(time).format('X');
+  } else if(moment(time, 'X').isValid()){
+    returnedObj.unix = time;
+    returnedObj.natural = moment(time).format('MMMM Do, YYYY');
+  }
   console.log(time);
+  console.log(moment(time, 'MMMM Do, YYYY').isValid());
 
 
   res.send(returnedObj);
